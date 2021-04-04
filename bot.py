d3613eba243cc5c8bbfab0fdde75db1fdf484837
vicyyn <nader_ouerdiane@hotmail.com>
2021-04-04 19:23:06 +0100

    updated

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ cg = CoinGeckoAPI()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('logged in as {0.user}'.format(client))
     await main()
 
 client.run(botToken,  reconnect = True)
